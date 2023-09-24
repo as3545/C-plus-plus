8124cd1b094fab8efa022d080fd107c19bdc6bb6
as3545 <86479682+as3545@users.noreply.github.com>
2023-09-24 22:08:48 +0530

    Replace.cpp

--- Replace.cpp
+++ Replace.cpp
@@ -1,23 +1,20 @@
 //Code
 
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-string replace(string s, char s1, char s2){
-    
+string replace(string s,char s1,char s2){
     for(int i=0;i<s.length();i++){
         if(s[i]==s1){
             s[i]=s2;
         }
-    }
-    return s;
+    } return s;
 }
-int  main(){
+int main(){
     string s;
     cin>>s;
-    char str1,str2;
-    cin>>str1;
-    cin>>str2;
-    
-    cout<<replace(s,str1,str2);
+    char s1, s2;
+    cin>>s1;
+    cin>>s2;
+    cout<<replace(s,s1,s2);
     return 0;
 }
